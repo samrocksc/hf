8b6a07fb15871ed2343f35df20db2b38aa1743e1
Sam C <samrocksc@gmail.com>
2023-08-21 17:38:29 +0200

    Adds cleanup of CSS and removes center alignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import './App.css';
-import { JsonInterpreter } from './SampleComponent';
-
-// export const sampleInput = {
-//   date: '2021-10-27T07:49:14.896Z',
-//   hasError: false,
-//   fields: [
-//     {
-//       id: '4c212130',
-//       prop: 'iban',
-//       value: 'DE81200505501265402568',
-//       hasError: false,
-//     },
-//   ],
-// };
+import "./App.css";
+import { JsonInterpreter } from "./SampleComponent";
 
 function App() {
   return (
-    <>
-      <div>
-        <JsonInterpreter />
-      </div>
-    </>
+    <div>
+      <h1>My JSON Interpreter</h1>
+      <JsonInterpreter />
+    </div>
   );
 }
 
